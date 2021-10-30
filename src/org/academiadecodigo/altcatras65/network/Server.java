364533c6ca8ec8b15a0e877684131d324090ceb8
jp.pereira <jpvp.pedro9@gmail.com>
2021-10-30 14:37:41 +0000

    add Server constructor, listen, createRoom and serve methods

--- src/org/academiadecodigo/altcatras65/network/Server.java
+++ src/org/academiadecodigo/altcatras65/network/Server.java
@@ -2,28 +2,73 @@ package org.academiadecodigo.altcatras65.network;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.academiadecodigo.altcatras65.game.room.Room;
+import org.academiadecodigo.altcatras65.game.room.RoomFactory;
 
 public class Server {
     private ServerSocket serverSocket;
-    private LinkedList roomList;
+    private LinkedList<Room> roomList;
+    private ExecutorService cachedPool;
+
+    /** Starts a new server and invokes method listen
+     *
+     * @param port
+     */
 
     public Server(int port) {
         try {
             serverSocket = new ServerSocket(port);
+            roomList = new LinkedList<>();
+            cachedPool = Executors.newCachedThreadPool();
+            createRoom();
+            listen();
         } catch (IOException e) {
             System.out.println(e);
         }
     }
 
     /** Listens to incoming clients and accepts them
-     *
+     *  Sends the clients to an existing room from roomList
      *
      */
 
     private void listen() {
         while(true) {
-
+            try {
+                serve(serverSocket.accept());
+            } catch (IOException e) {
+                System.out.println(e);
+            }
         }
+    }
+
+    /** Creates a new room
+     *
+     */
+
+    private void createRoom() {
+        Room room = RoomFactory.createRoom();
+        roomList.add(room);
+        cachedPool.submit(room);
+
+    }
+
+    /** Serves the client to a room
+     *
+     *
+     */
+
+    private void serve(Socket clientSocket) {
+        if(roomList.getLast().getPlayers() >= roomList.getLast().getMaxRoomSize) {
+            createRoom();
+            serve(clientSocket);
+        }
+
+        roomList.getLast().getPlayer(clientSocket);
     }
 }
