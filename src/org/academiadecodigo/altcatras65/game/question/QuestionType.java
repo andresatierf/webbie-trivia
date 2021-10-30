1816289fa294a116b43234ad1e4f7cb80351c5e6
andresatierf <andrecfreitas58gmail.com>
2021-10-30 18:22:09 +0000

    Add question color

--- src/org/academiadecodigo/altcatras65/game/question/QuestionType.java
+++ src/org/academiadecodigo/altcatras65/game/question/QuestionType.java
@@ -1,16 +1,22 @@
 package org.academiadecodigo.altcatras65.game.question;
 
+import org.academiadecodigo.altcatras65.game.Colors;
+
+import java.awt.*;
+
 public enum QuestionType {
-    HIGH(50,-25),
-    MEDIUM(30, -15),
-    LOW(10, -5);
+    HIGH(50,-25, Colors.RED),
+    MEDIUM(30, -15, Colors.YELLOW),
+    LOW(10, -5, Colors.WHITE);
 
     private int winValue;
     private int loseValue;
+    private Colors color;
 
-    QuestionType(int winValue, int loseValue) {
+    QuestionType(int winValue, int loseValue, Colors color) {
         this.winValue = winValue;
         this.loseValue = loseValue;
+        this.color = color;
     }
 
     //region Getters and Setters
@@ -31,6 +37,10 @@ public enum QuestionType {
         this.loseValue = loseValue;
     }
 
-    //endregion
+    public Colors getColor() {
+        return color;
+    }
+
+//endregion
 
 }
