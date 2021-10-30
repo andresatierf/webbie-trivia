a4882d3cda58b456f9522b8a00849b55d57741ae
MarioWork <mariovieira.work@hotmail.com>
2021-10-30 14:29:56 +0000

    Room Creation Logic

--- src/org/academiadecodigo/altcatras65/game/room/Room.java
+++ src/org/academiadecodigo/altcatras65/game/room/Room.java
@@ -1,4 +1,47 @@
 package org.academiadecodigo.altcatras65.game.room;
 
-public class Room {
+import org.academiadecodigo.altcatras65.game.ThemeType;
+import org.academiadecodigo.altcatras65.game.player.Player;
+import org.academiadecodigo.altcatras65.game.player.PlayerFactory;
+import org.academiadecodigo.altcatras65.game.question.QuestionFactory;
+
+import java.awt.*;
+import java.net.Socket;
+import java.util.List;
+
+public class Room implements Runnable {
+    private List<Player> players;
+    private int maxQuestions;
+    private int maxRoomSize;
+    private ThemeType theme;
+
+    public void init() {
+
+    }
+
+    public void start() {
+
+    }
+
+
+    private void nextQuestion() {
+
+    }
+
+    private void awaitAnswers() {
+
+    }
+
+    private void getQuestions() {
+        QuestionFactory.createQuestions(theme);
+    }
+
+    public void addPlayer(Socket socket, String name, Color color) {
+        PlayerFactory.createPlayer(socket, name, color);
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
