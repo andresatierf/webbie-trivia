1a72548f4a35e6c9f09042716b88735d87019335
MarioWork <mariovieira.work@hotmail.com>
2021-10-30 14:29:47 +0000

    Question Creation Logic

--- src/org/academiadecodigo/altcatras65/game/question/Question.java
+++ src/org/academiadecodigo/altcatras65/game/question/Question.java
@@ -1,4 +1,29 @@
 package org.academiadecodigo.altcatras65.game.question;
 
+import org.academiadecodigo.altcatras65.game.ThemeType;
+
+import java.util.List;
+
 public class Question {
+    private String description;
+    private String correctAnswer;
+    private List<String> possibleAnswers;
+    private QuestionType type;
+    private ThemeType theme;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public List<String> getPossibleAnswers() {
+        return possibleAnswers;
+    }
+
+    public QuestionType getType() {
+        return type;
+    }
 }
