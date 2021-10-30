eea307e3fc4e761bd01a079ef4766dddfb96e40b
jp.pereira <jpvp.pedro9@gmail.com>
2021-10-30 13:34:57 +0000

    add ServerSocket, roomList

--- src/org/academiadecodigo/altcatras65/network/Server.java
+++ src/org/academiadecodigo/altcatras65/network/Server.java
@@ -1,4 +1,29 @@
 package org.academiadecodigo.altcatras65.network;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.LinkedList;
+
 public class Server {
+    private ServerSocket serverSocket;
+    private LinkedList roomList;
+
+    public Server(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    /** Listens to incoming clients and accepts them
+     *
+     *
+     */
+
+    private void listen() {
+        while(true) {
+
+        }
+    }
 }
