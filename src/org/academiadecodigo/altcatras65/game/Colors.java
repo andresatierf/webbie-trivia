f4455ef0780c66f01a3b45ab64089b9cadfb01cb
andresatierf <andrecfreitas58gmail.com>
2021-10-31 17:44:30 -0100

    Remove awt colors

--- src/org/academiadecodigo/altcatras65/game/Colors.java
+++ src/org/academiadecodigo/altcatras65/game/Colors.java
@@ -1,28 +1,19 @@
 package org.academiadecodigo.altcatras65.game;
 
-import java.awt.*;
-
 public enum Colors {
-    WHITE(Color.WHITE, "White", "\u001B[0m"),
-    BLACK(Color.BLACK, "Black", "\u001B[30m"),
-    RED(Color.RED, "Red", "\u001B[31m"),
-    GREEN(Color.GREEN, "Green", "\u001B[32m"),
-    BLUE(Color.BLUE, "Blue", "\u001B[34m"),
-    YELLOW(Color.YELLOW, "Yellow", "\u001B[33m"),
-    CYAN(Color.CYAN, "Cyan", "\u001B[36m");
+    WHITE("White", "\u001B[0m"),
+    RED("Red", "\u001B[31m"),
+    GREEN("Green", "\u001B[32m"),
+    BLUE("Blue", "\u001B[34m"),
+    YELLOW("Yellow", "\u001B[33m"),
+    CYAN("Cyan", "\u001B[36m");
 
-    private Color color;
     private String name;
     private String asciiColor;
 
-    Colors(Color color, String name, String asciiColor) {
-        this.color = color;
+    Colors(String name, String asciiColor) {
         this.name = name;
         this.asciiColor = asciiColor;
-    }
-
-    public Color getColor() {
-        return color;
     }
 
     public String getName() {
